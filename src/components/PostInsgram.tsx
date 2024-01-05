5015aa0ebb843b46bd4a86a4a0e096303952b413
Leonardo Santos <leonardo.santos574@hotmail.com>
2024-01-04 21:08:49 -0300

    again

--- src/components/PostInsgram.tsx
+++ src/components/PostInsgram.tsx
@@ -8,7 +8,7 @@ import postImg6 from '../../public/image-instagram-post-6.jpg'
 export function PostInstagram() {
   return (
     <div className="mx-auto grid grid-cols-2 gap-2 md:grid md:grid-cols-3 md:gap-4">
-      <div className="h-40 w-40 bg-bege-400 transition duration-200 hover:opacity-70 md:h-64 md:w-64 lg:h-80 lg:w-80">
+      <div className="h-36 w-36 bg-bege-400 transition duration-200 hover:opacity-70 md:h-64 md:w-64 lg:h-80 lg:w-80">
         <a
           href="https://www.instagram.com/p/CT97awDr8Pw/?img_index=1"
           target="_blank"
@@ -17,7 +17,7 @@ export function PostInstagram() {
           <img src={postImg1} className="h-full w-full object-cover" />
         </a>
       </div>
-      <div className="h-40 w-40 bg-bege-400 transition duration-200 hover:opacity-70 md:h-64 md:w-64 lg:h-80 lg:w-80">
+      <div className="h-36 w-36 bg-bege-400 transition duration-200 hover:opacity-70 md:h-64 md:w-64 lg:h-80 lg:w-80">
         <a
           href="https://www.instagram.com/p/CQZGx78Heon/"
           target="_blank"
@@ -26,7 +26,7 @@ export function PostInstagram() {
           <img src={postImg2} className="h-full w-full object-cover" />
         </a>
       </div>
-      <div className="h-40 w-40 bg-bege-400 transition duration-200 hover:opacity-70 md:h-64 md:w-64 lg:h-80 lg:w-80">
+      <div className="h-36 w-36 bg-bege-400 transition duration-200 hover:opacity-70 md:h-64 md:w-64 lg:h-80 lg:w-80">
         <a
           href="https://www.instagram.com/p/CQHYB9nngDm/?img_index=1"
           target="_blank"
@@ -35,7 +35,7 @@ export function PostInstagram() {
           <img src={postImg3} className="h-full w-full object-cover" />
         </a>
       </div>
-      <div className="h-40 w-40 bg-bege-400 transition duration-200 hover:opacity-70 md:h-64 md:w-64 lg:h-80 lg:w-80">
+      <div className="h-36 w-36 bg-bege-400 transition duration-200 hover:opacity-70 md:h-64 md:w-64 lg:h-80 lg:w-80">
         <a
           href="https://www.instagram.com/p/COBiWTSnQ0b/"
           target="_blank"
@@ -44,7 +44,7 @@ export function PostInstagram() {
           <img src={postImg4} className="h-full w-full object-cover" />
         </a>
       </div>
-      <div className="h-40 w-40 bg-bege-400 transition duration-200 hover:opacity-70 md:h-64 md:w-64 lg:h-80 lg:w-80">
+      <div className="h-36 w-36 bg-bege-400 transition duration-200 hover:opacity-70 md:h-64 md:w-64 lg:h-80 lg:w-80">
         <a
           href="https://www.instagram.com/p/CmfNjiWu8pB/"
           target="_blank"
@@ -53,7 +53,7 @@ export function PostInstagram() {
           <img src={postImg5} className="h-full w-full object-cover" />
         </a>
       </div>
-      <div className="h-40 w-40 bg-bege-400 transition duration-200 hover:opacity-70 md:h-64 md:w-64 lg:h-80 lg:w-80">
+      <div className="h-36 w-36 bg-bege-400 transition duration-200 hover:opacity-70 md:h-64 md:w-64 lg:h-80 lg:w-80">
         <a
           href="https://www.instagram.com/p/CwVwY0xqVz4/?img_index=1"
           target="_blank"
